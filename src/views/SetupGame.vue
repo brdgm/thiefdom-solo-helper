5b71653be33853cab6894db1bf9a911a83f214dd
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-01-04 20:03:31 +0100

    setup game

--- src/views/SetupGame.vue
+++ src/views/SetupGame.vue
@@ -3,8 +3,24 @@
 
   <PlayersSetup/>
 
+  <h3 class="mt-3">{{t('setup.setup.title')}}</h3>
+  <ul>
+    <li v-html="t('setup.setup.gameBoard')"></li>
+    <li v-html="t('setup.setup.playerBoard')"></li>
+    <li v-html="t('setup.setup.rivalThieves')"></li>
+    <li v-html="t('setup.setup.draftEquipment')"></li>
+    <li v-html="t('setup.setup.placeHideout')"></li>
+  </ul>
+
+  <h3 class="mt-3">{{t('setup.ruleChanges.title')}}</h3>
+  <ul>
+    <li v-html="t('setup.ruleChanges.townsfolkMovement')"></li>
+    <li v-html="t('setup.ruleChanges.enterLocationRival')"></li>
+    <li v-html="t('setup.ruleChanges.getArrested')"></li>
+  </ul>
+
   <button class="btn btn-primary btn-lg mt-4" @click="setupBot()">
-    {{t('setupBot.title')}}
+    {{t('action.startGame')}}
   </button>
 
   <FooterButtons endGameButtonType="abortGame"/>
@@ -26,12 +42,11 @@ export default defineComponent({
   setup() {
     const { t } = useI18n()
     const state = useStateStore()
-
     return { t, state }
   },
   methods: {
     setupBot() : void {
-      this.$router.push('/setupBot')
+      this.$router.push('/round/1/turn/1')
     }
   }
 })
